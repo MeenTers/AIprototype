f73ca66f2f5545260e20a652c9b2c867037aa599
MeenTers <meenthanapon@kkumail.com>
2022-01-11 11:43:45 +0700

    add Popen

--- testsub.py
+++ testsub.py
@@ -1,5 +1,14 @@
 import subprocess #สำหรับ รัน terminal command
 
 if __name__ == "__main__":
+    ##basic
     subprocess.run(["ls","-l"])
-    subprocess.run(["python","python101.py","9","--x","11"])
+    for i in [2,5,6,8]:
+        subprocess.run(["python","python101.py","9","--x",f"{i}"])
+    
+    ## use output of subprocess
+    pro = subprocess.Popen(["ls","-l"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = pro.communicate()
+    print(out)
+
+    ##HW ให้ print เฉพาะ ตัวเลขผลลัพธ์การคูร โดยไม่ต้องแก้โปรแกรมเก่า
