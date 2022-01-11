bcc5c3671101ad7b71c77df3bb4adf802ddcdf0d
MeenTers <meenthanapon@kkumail.com>
2022-01-11 11:20:05 +0700

    test

--- testsub.py
+++ testsub.py
@@ -2,4 +2,4 @@ import subprocess #สำหรับ รัน terminal command
 
 if __name__ == "__main__":
     subprocess.run(["ls","-l"])
-    subprocess.run(["python101.py","9","--x","11"])
+    subprocess.run(["python","python101.py","9","--x","11"])
