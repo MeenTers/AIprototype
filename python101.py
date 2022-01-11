9dbb032118a7f256060fdc56b5936f6af4ca6ab5
MeenTers <meenthanapon@kkumail.com>
2022-01-11 09:26:06 +0700

    .\python101.py

--- python101.py
+++ python101.py
@@ -1,5 +1,13 @@
+import argparse #สำหรับ รับ input จากภายนอก
+import subprocess #สำหรับรัน terminal command 
+
+#import flask # สำหรับทำ web app และ web service api
+
+
 def print_other():
     print('something else')
 
 if __name__=="__main__":
-    print('test main function')
+    x = 2
+    y = 3
+    print(f'calculate {x} x {y} = {x*y}')
